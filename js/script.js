9923205bc06c8cc077dc3f9b41258a09d2312ec0
hyor <39582302+hyor@users.noreply.github.com>
2018-12-13 14:30:35 +0900

    revise

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 $(function(){
   $('.main_view .view').bxSlider({auto:true, mode:'fade', controls:false, pager:false});
+  $('.enjoy_view .enjoy_slider').bxSlider({auto:true, mode:'fade', maxSlides:4, slideWidth:340, moveSlides:1});
 });
