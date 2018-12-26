0a485e1a2b3512992abd4cb3568730542299216d
hyor <39582302+hyor@users.noreply.github.com>
2018-12-26 16:45:53 +0900

    revise

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function(){
   $('.main_view .view').bxSlider({auto:true, mode:'fade', controls:false, pager:false});
-  $('.enjoy_view .enjoy_slider').bxSlider({auto:true, maxSlides:4, minSlides:4, slideWidth:300, moveSlides:1, prevText:"", nextText:""});
-  $('#menu .menu_slider').bxSlider({auto:true, maxSlides:4, slideWidth:204, minSlides:2, moveSlides:1, prevText:"", nextText:""});
+  $('.enjoy_view .enjoy_slider').bxSlider({auto:true, maxSlides:4, minSlides:1, slideWidth:300, moveSlides:1, prevText:"", nextText:""});
+  $('#menu .menu_slider').bxSlider({auto:true, maxSlides:4, slideWidth:204, minSlides:1, moveSlides:1, prevText:"", nextText:""});
   $('.mcdrive_ani').scrollToGiveClass({class:"active", baseline:'bottom'});
 });
